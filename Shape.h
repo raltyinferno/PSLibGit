09610bef3bb88cb9dcaa43f202a8a2bf576f5cc2
Tristan Craddick <tjcraddick@alaska.edu>
2016-04-13 15:19:51 -0800

    Extremely basic outline for a shape class.

---
+++ Shape.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef SHAPE_H_INCLUDED
+#define SHAPE_H_INCLUDED
+
+#include <string>
+using std::string;
+
+class Shape
+{
+	/********************Variables*********************/
+
+public:
+	struct BoundingBox 
+	{
+		float xTop;
+		float xBottom;
+		float yTop;
+		float yBottom;
+	};
+
+private:
+
+
+
+	/*********************Function Constructors**************************/
+
+
+};
+
+
+
+
+#endif
