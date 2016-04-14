6b577599b7cfc8018e4c5a58685176a009de38f7
Kai Davids Schell <kdavidsschell@alaska.edu>
2016-04-13 17:48:48 -0800

    update to shape adding decorator and rotate (not compiling atm)

--- Shape.h
+++ Shape.h
@@ -62,5 +62,34 @@ private:
 	string center;
 };
 
+class Decorator : public Shape
+{
+public:
+	Decorator() {}
+	~Decorator() {}
+	virtual string draw() = 0;
+};
+class Rotateor : public Decorator
+{
+public:
+	Rotateor(int ang, const Shape & shp)
+	{
+		ostringstream converter;
+		converter << angle;
+		angle = converter.str();
+		passed_shape = shp.draw();
+	}
+	~Rotateor() {}
+	string draw()
+	{
+		string start = " gsave rotate ";
+		string rot = " rotate " + angle +" ";
+		string end = " grestore ";
+		return start + rot + passed_shape + end;
+	}
 
+private:
+	string angle;
+	string passed_shape;
+};
 #endif
